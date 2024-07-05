586e4f928d36e3c8c4ffc0cb23e43e4ae3af3ea5
Lucas Mas <lucas_mas@msn.com>
2024-07-06 00:22:00 +0200

    Added Oil. Clean init.lua by splitting more into files.

---
+++ lua/plugins/catpuccin.lua
@@ -0,0 +1,13 @@
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  init = function()
+    vim.cmd.colorscheme 'catppuccin'
+  end,
+  config = function()
+    require('catppuccin').setup {
+      flavour = 'mocha',
+    }
+  end,
+}
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,3 @@
--- Adds git related signs to the gutter, as well as utilities for managing changes
--- NOTE: gitsigns is already included in init.lua but contains only the base
--- config. This will add also the recommended keymaps.
-
 return {
   {
     'lewis6991/gitsigns.nvim',
@@ -56,6 +52,14 @@ return {
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
         map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
       end,
+
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
     },
   },
 }
