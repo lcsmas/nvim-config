52974e3b493d96b06f303af5b69f76a521cfa7c9
Lucas Mas <lucas_mas@msn.com>
2025-02-04 16:37:54 +0100

    increase oil lsp file method timeout

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,4 +4,9 @@ return {
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   vim.keymap.set('n', '\\', ':Oil<CR>'),
+  config = function()
+    require('oil').setup {
+      lsp_file_methods = { timeout_ms = 10000 },
+    }
+  end,
 }
