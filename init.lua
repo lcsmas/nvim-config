903ab261f2ad0f39429f95f5428557e3ccd499f2
Lucas Mas <lucas_mas@msn.com>
2025-02-04 18:42:26 +0100

    center when C-d and C-u, disable neoscroll

--- init.lua
+++ init.lua
@@ -106,8 +106,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- vim.keymap.set('n', '<leader>c', '<cmd>vnew | term<CR>', { desc = 'New vertical terminal' })
 vim.keymap.set('n', '<leader>n', '<cmd>vnew<CR>', { desc = 'New vertical' })
+vim.keymap.set({ 'n', 'v' }, '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<C-u>', '<C-u>zz', { noremap = true })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
