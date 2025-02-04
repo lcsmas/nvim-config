026b000c9453feae62e67d1dc4ea9d4298452d6b
Lucas Mas <lucas_mas@msn.com>
2025-02-04 16:37:18 +0100

    disable lsp file operation

--- lua/plugins/lsp-file-operations.lua
+++ lua/plugins/lsp-file-operations.lua
@@ -1,12 +1,14 @@
 return {
-  {
-    'antosha417/nvim-lsp-file-operations',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-neo-tree/neo-tree.nvim',
-    },
-    config = function()
-      require('lsp-file-operations').setup()
-    end,
-  },
+  -- {
+  --   'antosha417/nvim-lsp-file-operations',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-neo-tree/neo-tree.nvim',
+  --   },
+  --   config = function()
+  --     require('lsp-file-operations').setup {
+  --       timeout_ms = 10000,
+  --     }
+  --   end,
+  -- },
 }
